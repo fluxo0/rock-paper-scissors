f48ab2b21a26a18a595c26d62d3bff49f8e5c13a
fluxo0 <180851775+fluxo0@users.noreply.github.com>
2025-02-14 21:42:50 -0300

    Add logic to play a single round

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,3 +1,15 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+console.log(humanSelection);
+console.log(computerSelection);
+
+playRound(humanSelection, computerSelection);
+
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
 
@@ -11,12 +23,56 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
 
 function getHumanChoice() {
-    let choice = prompt("Type rock, paper or scissors!");
+    let choice = prompt("Type rock, paper or scissors!", "Type here");
 
     return choice.toLowerCase();
 }
 
-console.log(getHumanChoice());
+
+function playRound(humanChoice, computerChoice) {
+    let humanChar = humanChoice.charAt(0);
+    humanChar = humanChar.toUpperCase();
+
+    let human = humanChar.concat(humanChoice.slice(1));
+
+    let computerChar = computerChoice.charAt(0);
+    computerChar = computerChar.toUpperCase();
+
+    let computer = computerChar.concat(computerChoice.slice(1));
+
+    if (humanChoice === computerChoice) {
+        console.log(`Tie! Both chose ${human}`);
+
+    } else if (humanChoice === "rock") {
+        if (computerChoice === "scissors") {
+            humanScore++;
+            console.log(`You win! ${human} beats ${computer}`);
+        } else {
+            computerScore++;
+            console.log(`You lose! ${computer} beats ${human}`);
+        }
+
+    } else if (humanChoice === "paper") {
+        if (computerChoice === "rock") {
+            humanScore++;
+            console.log(`You win! ${human} beats ${computer}`);
+        } else {
+            computerScore++;
+            console.log(`You lose! ${computer} beats ${human}`);
+        }
+
+    } else if (humanChoice === "scissors") {
+        if (computerChoice === "paper") {
+            humanScore++;
+            console.log(`You win! ${human} beats ${computer}`);
+        } else {
+            computerScore++;
+            console.log(`You lose! ${computer} beats ${human}`);
+        }
+    
+    } else {
+        console.log("Chose between Rock, Paper or Scissors");
+    }
+}
